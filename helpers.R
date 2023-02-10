9f797d719661bfea2690fb4a0c274ca05df497af
Tony Wu <58774448+w-tony@users.noreply.github.com>
2023-02-10 16:03:42 +1300

    Delete helpers.R

--- helpers.R
+++
@@ -1,2 +0,0 @@
-if(!exists("interRAI.df")) interRAI.df <- read_csv("interRAI_final.csv")
-if(!exists("hospitalisation.df")) hospitalisation.df <- read_csv("Z:/Hospitalisation data/Hospitalisation data.csv")
